959b4e260169c0b390651ba2f4c565b304ce8d1f
NguyenThiNgocChau <nguyenngochau764@gmail.com>
2025-03-03 07:44:29 +0700

    init project

---
+++ src/main/java/com/example/NguyenThiNgocChau/NguyenThiNgocChauApplication.java
@@ -0,0 +1,13 @@
+package com.example.NguyenThiNgocChau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenThiNgocChauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenThiNgocChauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NguyenThiNgocChau/NguyenThiNgocChauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NguyenThiNgocChau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenThiNgocChauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
